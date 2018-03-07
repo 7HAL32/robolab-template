c76792538fe0ce2ef552a3de1fb509c68d6323bb
Kilian Koeltzsch <me@kilian.io>
2018-03-07 15:51:42 +0100

    s/assertFail/fail
    assertFail doesn't exist, fail does

--- src/planettest.py
+++ src/planettest.py
@@ -31,26 +31,26 @@ class TestPlanet(PlanetTestCase):
     def test_integrity(self):
         # were all paths added correctly to the planet
         # check if add_path() works by using get_paths()
-        self.assertFail('implement me!')
+        self.fail('implement me!')
 
     def test_empty_planet(self):
-        self.assertFail('implement me!')
+        self.fail('implement me!')
 
     def test_target_not_reachable(self):
-        self.assertFail('implement me!')
+        self.fail('implement me!')
 
     def test_shortest_path(self):
         # at least 2 possible paths
-        self.assertFail('implement me!')
+        self.fail('implement me!')
 
     def test_same_length(self):
         # at least 2 possible paths with the same weight
-        self.assertFail('implement me!')
+        self.fail('implement me!')
 
     def test_shortest_path_with_loop(self):
         # does the shortest path algorithm loop infinitely?
         # there is a shortest path
-        self.assertFail('implement me!')
+        self.fail('implement me!')
 
     def test_target_not_reachable_with_loop(self):
         # does the shortest path algorithm loop infinitely?
