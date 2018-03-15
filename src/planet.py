144475b1df6ec2eaecd41a15b004051b3710e1aa
Lutz Thies <lutz.thies@tu-dresden.de>
2018-03-15 19:20:18 +0100

    unified functions signatures of interface, planet test cases should be more clear now

--- src/planet.py
+++ src/planet.py
@@ -2,7 +2,7 @@
 
 from enum import IntEnum, unique
 from typing import List, Optional, Tuple, Dict
-# IMPORTANT NOTE: DO NOT IMPORT THE ev3dev.ev3 MODULE IN THIS FILE
+# IMPORTANT NOTE: DO NOT IMPORT THE ev3dev.ev3 MODULE IN THIS FILE)
 
 
 @unique
@@ -25,38 +25,48 @@ Weight = int
 
 
 class Planet:
-    """ Contains the representation of the map and provides certain functions to manipulate it according to the specifications """
+    """ 
+    Contains the representation of the map and provides certain functions to manipulate it according to the specifications 
+    """
 
     def __init__(self):
         """ Initializes the data structure """
         self.target = None
 
-    # example: add_path((0, 3, Direction.NORTH), (0, 3, Direction.WEST), 1)
-    def add_path(self, start: Tuple[int, int, Direction], target: Tuple[int, int, Direction], weight: int):
-        """ Adds a path defined by its start and end coordinates to the map and assigns the weight to it """
-        pass
+    def add_path(self, start: Tuple[Tuple[int, int], Direction], target: Tuple[Tuple[int, int], Direction], weight: int):
+        """ 
+        Adds a bidirectional path defined between the start and end coordinates to the map and assigns the weight to it 
 
-    """ 
         example: 
-        get_paths() returns: { 
-                                (0, 3): {
-                                            Direction.NORTH: ((0, 3), Direction.WEST, 1), 
-                                            Direction.EAST: ((1, 3), Direction.WEST, 2) 
-                                        },
-                                (1, 3): {
-                                            Direction.WEST: ((0, 3), Direction.EAST, 2), 
-                                            ... 
-                                        }, 
-                                ...
-                              }
-    """
+            add_path(((0, 3), Direction.NORTH), ((0, 3), Direction.WEST), 1)
+        """
+        pass
 
     def get_paths(self) -> Dict[Tuple[int, int], Dict[Direction, Tuple[Tuple[int, int], Direction, Weight]]]:
-        """ Returns all paths """
+        """ 
+        Returns all paths 
+
+        example: 
+            get_paths() returns: { 
+                                    (0, 3): {
+                                                Direction.NORTH: ((0, 3), Direction.WEST, 1), 
+                                                Direction.EAST: ((1, 3), Direction.WEST, 2) 
+                                            },
+                                    (1, 3): {
+                                                Direction.WEST: ((0, 3), Direction.EAST, 2), 
+                                                ... 
+                                            }, 
+                                    ...
+                                  }
+        """
         pass
 
-    # example: shortest_path((0,0), (2,2)) returns: [(0, 0, Direction.East), (1, 0, Direction.North)]
-    # example: shortest_path((0,0), (1,2)) returns: None
-    def shortest_path(self, start: Tuple[int, int], target: Tuple[int, int]) -> Optional[List[Tuple[int, int, Direction]]]:
-        """ Returns a shortest path between two nodes """
+    def shortest_path(self, start: Tuple[int, int], target: Tuple[int, int]) -> Optional[List[Tuple[Tuple[int, int], Direction]]]:
+        """ 
+        Returns a shortest path between two nodes 
+
+        examples: 
+            shortest_path((0,0), (2,2)) returns: [((0, 0), Direction.EAST), ((1, 0), Direction.NORTH)]
+            shortest_path((0,0), (1,2)) returns: None
+        """
         pass
--- src/planettest.py
+++ src/planettest.py
@@ -4,16 +4,13 @@ import unittest
 from planet import Direction, Planet
 
 
-class PlanetTestCase(unittest.TestCase):
+class ExampleTestPlanet(unittest.TestCase):
     def setUp(self):
-        self.planet = Planet()
-
-
-class TestPlanet(PlanetTestCase):
-    """
-        model a planet and implement test cases
+        """
+        Instantiates the planet data structure and fills it with paths
 
         example planet:
+
         +--+
         |  |
         +-0,3------+
@@ -26,38 +23,62 @@ class TestPlanet(PlanetTestCase):
            |
         (start)
 
-    """
+        """
 
-    def test_integrity(self):
-        # were all paths added correctly to the planet
-        # check if add_path() works by using get_paths()
-        self.assertFail('implement me!')
+        # set your data structure
+        self.planet = Planet()
 
-    def test_empty_planet(self):
-        self.assertFail('implement me!')
-
-    def test_target_not_reachable(self):
-        self.assertFail('implement me!')
-
-    def test_shortest_path(self):
-        # at least 2 possible paths
-        self.assertFail('implement me!')
-
-    def test_same_length(self):
-        # at least 2 possible paths with the same weight
-        self.assertFail('implement me!')
-
-    def test_shortest_path_with_loop(self):
-        # does the shortest path algorithm loop infinitely?
-        # there is a shortest path
-        self.assertFail('implement me!')
+        # add the paths
+        self.planet.add_path(((0, 0), Direction.NORTH), ((0, 1), Direction.SOUTH), 1)
+        self.planet.add_path(((0, 1), Direction.WEST), ((0, 0), Direction.WEST), 1)
 
     def test_target_not_reachable_with_loop(self):
         # does the shortest path algorithm loop infinitely?
         # there is no shortest path
-        self.planet.add_path((0, 0, Direction.NORTH), (0, 1, Direction.SOUTH), 1)
-        self.planet.add_path((0, 1, Direction.WEST), (0, 0, Direction.WEST), 1)
         self.assertIsNone(self.planet.shortest_path((0, 0), (1, 2)))
+
+
+class YourFirstTestPlanet(unittest.TestCase):
+    def setUp(self):
+        """
+        Instantiates the planet data structure and fills it with paths
+
+        MODEL YOUR TEST PLANET HERE (if you'd like):
+
+        """
+        # set your data structure
+        self.planet = Planet()
+
+        # ADD YOUR PATHS HERE:
+        # self.planet.add_path(...)
+
+    def test_integrity(self):
+        # were all paths added correctly to the planet
+        # check if add_path() works by using get_paths()
+        self.fail('implement me!')
+
+    def test_empty_planet(self):
+        self.fail('implement me!')
+
+    def test_target_not_reachable(self):
+        self.fail('implement me!')
+
+    def test_shortest_path(self):
+        # at least 2 possible paths
+        self.fail('implement me!')
+
+    def test_same_length(self):
+        # at least 2 possible paths with the same weight
+        self.fail('implement me!')
+
+    def test_shortest_path_with_loop(self):
+        # does the shortest path algorithm loop infinitely?
+        # there is a shortest path
+        self.fail('implement me!')
+
+    def test_target_not_reachable_with_loop(self):
+        # there is no shortest path
+        self.fail('implement me!')
 
 
 if __name__ == "__main__":
